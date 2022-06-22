48c014be266afed4d5dc77db89f6bc08cce0412b
Peter Brännström <54285305+Brannstroom@users.noreply.github.com>
2022-06-22 22:49:16 +0200

    Update lae.js

--- twscripts/lae.js
+++ twscripts/lae.js
@@ -7,8 +7,8 @@ if(document.location.href.endsWith("am_farm")) {
         window.location.reload();
     }
     else {
-        var version = "1.13.1 Fixed By Ibra";
-        var scriptName = "LA Enhancer (1.13.1) - Fixed by Ibra Gonza II";
+        var version = "1.13.1 - Modified by Brannstrom";
+        var scriptName = "LA Enhancer (1.13.1) - Modified by Brannstrom";
         var scriptURL = "https://scripts.ibragonza.nl/enhancer/";
         var updateNotesURL = "https://forum.tribalwars.net/index.php?threads/ntoombs19s-fa-filter.266604/pae-15#post-7053294";
         var working = true;
